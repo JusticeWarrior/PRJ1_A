8810e544c691dc3b6518e82b875458b7c35878f6
Eric Matthew Colter <coltere@ecegrid-thin1.ecn.purdue.edu>
2015-01-30 23:30:44 -0500

    Squared up FEL.h real good

--- FEL.h
+++ FEL.h
@@ -8,4 +8,14 @@ typedef Struct FEL_st{
   int Lambda0;
   int Lambda1;
   int Mu;
-}
+} FEL;
+
+void FEL_GenerateNewArrival(FEL futureEvents, int  priority);
+
+void FEL_GenerateNewDeparture(FEL futureEvents);
+
+void FEL_AddEvent(FEL futureEvents, Event event);
+
+int FEL_isEmpty(FEL futureEvents);
+
+Event* FEL_popEvent(FEL futureEvents);
