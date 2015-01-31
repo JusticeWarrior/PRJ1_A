0b7388fdc9915928de761f8239cec70139440f91
Jordan Huffaker <jordhuff@gmail.com>
2015-01-31 00:16:09 -0500

    Created an outline for Event and fixed a bug in ListNode.

---
+++ Event.h
@@ -0,0 +1,15 @@
+#ifndef EVENT_INCLUDED
+#define EVENT_INCLUDED
+
+#include <stdlib.h>
+#include "FEL.h"
+#include "ListNode.h"
+#include "Server.h"
+#include "Queue.h"
+#include "SimulationData.h"
+
+typedef struct Event_st{
+	int gsdag;
+} Event;
+
+#endif /* EVENT_INCLUDED */
