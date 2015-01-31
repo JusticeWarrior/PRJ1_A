b0da26ca18befae25f2f1b7a063459ecc5172e46
Jordan Huffaker <jordhuff@gmail.com>
2015-01-31 16:46:45 -0500

    An event comparison function is now declared. The function compares against event times.

--- Event.h
+++ Event.h
@@ -18,4 +18,9 @@ Event* Event_Create(char type, int priority, int time);
 // Destroys the current instance of Event.
 void Event_Destroy(Event* event);
 
+// Compares two events according to their time. Returns a positive integer if
+// event1 has a lower time than event2, a negative integer if event2 has a lower
+// time than event1, and 0 if they are equal.
+int Event_CompTime(Event* event1, Event* event2);
+
 #endif /* EVENT_INCLUDED */
