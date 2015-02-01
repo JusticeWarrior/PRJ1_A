984c51a136ad9c4a04203d1a39b1ca3fc15a37e0
Jordan Huffaker <jordhuff@gmail.com>
2015-01-31 23:50:38 -0500

    Queue's can be created and destroyed.

--- Queue.c
+++ Queue.c
@@ -1,14 +1,30 @@
 #include "Queue.h"
 
-Queue* Queue_Create(Queue* queue)
+Queue* Queue_Create()
 {
-	return NULL;
+	Queue* queue = malloc(sizeof(Queue));
+
+	queue->Count = DEFAULTCOUNT;
+	queue->Head = NULL;
+	queue->Tail = NULL;
+
+	return queue;
 }
 
-void Queue_Destroy(Queue* queue) { }
+void Queue_Destroy(Queue* queue)
+{
+	if (queue == NULL)
+		return;
+
+	ListNode_DestroyList(queue->Head);
+
+	free(queue);
+}
 
 ListNode* Queue_Append(Queue* queue, ListNode* node)
 {
+	
+
 	return NULL;
 }
 
--- Queue.h
+++ Queue.h
@@ -6,19 +6,21 @@ typedef struct Queue_st Queue;
 
 #include "ListNode.h"
 
+#define DEFAULTCOUNT 0
+
 struct Queue_st{
 	int Count;
 	ListNode* Head;
 	ListNode* Tail;
 };
 
-// Creates and instance of a queue with a default count of 0 and null ListNode*.
-Queue* Queue_Create(Queue* queue);
+// Creates an instance of a queue with a default count of 0 and null ListNode*.
+Queue* Queue_Create();
 
 // Destroys the current instance of queue and all data that it still contains.
 void Queue_Destroy(Queue* queue);
 
-// Calls ListNode_Append to append the node to the front of the queue's head.
+// Calls ListNode_AppendTail to append the node to the tail of the queue.
 ListNode* Queue_Append(Queue* queue, ListNode* node);
 
 // Pops the head node off of the queue and returns it as output.
