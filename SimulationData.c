132ae02eec222f1c373c3a8922eb47bccae2a617
Eric Matthew Colter <coltere@ecegrid-thin1.ecn.purdue.edu>
2015-02-01 01:26:37 -0500

    Made all the functions for simulation data

--- SimulationData.c
+++ SimulationData.c
@@ -1,1 +1,35 @@
 #include "SimulationData.h"
+
+
+SimulationData* SimulationData_Create()
+{
+  SimulationData* simData = malloc(sizeof(SimulationData));
+  simData -> WaitingTime = malloc(sizeof(int)*PRIORITY_LEVELS);
+
+  simData -> CPUTime = 0;
+  simData -> CurrentTime = 0; 
+ 
+  return simData;
+}
+
+
+
+void SimulationData_Destroy(SimulationData* simData)
+{
+  free(simData -> WaitingTime);
+  free(simData);
+}
+
+float SimulationData_AverageWait(SimulationData* simData, int priority)
+{
+  float wait = simData -> WaitingTime[priority];
+  float average = wait / ((float)(simData->CurrentTime) );
+  return average;  
+}
+
+float SimulationData_Utilization(SimulationData* simData)
+{
+  float cpu = simData -> CPUTime;
+  float average = cpu / ((float)(simData->CurrentTime));
+  return average;  
+}
--- SimulationData.h
+++ SimulationData.h
@@ -14,8 +14,7 @@ struct SimulationData_st{
   int CurrentTime; //The current time of the simulation
  
   //The cumulative time spent on the queue for elements of each priority
-  int WaitingTime0;
-  int WaitingTime1;
+  int* WaitingTime;
 
   //The amount of time units so far, the CPU has spent handling a task
   int CPUTime;
@@ -27,4 +26,9 @@ SimulationData* SimulationData_Create();
 //Destroy a SimulationData structure
 void SimulationData_Destroy(SimulationData* data);
 
+//Calculate the average wait time for jobs of the given priority level
+float SimulationData_AverageWait(SimulationData* simData, int priority);
+
+//Calculate the average time the CPU has spent busy
+float SimulationData_Utilization(SimulationData* simData);
 #endif /* SIMULATIONDATA_INCLUDED */
