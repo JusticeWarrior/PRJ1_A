fd3fe715b53f9adbdaf32eff5a9ccceda8bf4da0
Eric Matthew Colter <coltere@ecegrid-thin1.ecn.purdue.edu>
2015-02-18 10:17:36 -0500

    FEL_Pop() is good to go.  Discovered bug in Event_Destroy function: the function shouldn't deincrement Task->SubTasks if the event being destroyed is a departure

--- UnitTestSet4.c
+++ UnitTestSet4.c
@@ -7,30 +7,67 @@
 #include <stdlib.h>
 
 #define INPUT_FILE "test1.txt"
+#define SUBTASKS 5
 
 int main(int argc, char** argv)
 {
+  //Delarations
   FEL* fel = FEL_Create(1,1,1,1,1);
   ListNode* list = NULL;
-  ListNode* tail = NULL
-  Task* task = Task_Create(3, 1, 3);
-  
+  ListNode* list2 = NULL;
+  ListNode* tail = NULL;
+  ListNode* tail2 = NULL;
+  Task* task = Task_Create(SUBTASKS, 1, SUBTASKS);
+  Task* task2 = Task_Create(SUBTASKS, 1, SUBTASKS);
+  int i; 
+
+  //Generate list and populate FEL with it
   list = ListNode_Create(Event_Create(ARRIVAL,0,0,1,task));
-  tail = ListNode_AppendTail(ListNode_Create(Event_Create(Arrival,0,0,2,task)),list);
-   tail = ListNode_AppendTail(ListNode_Create(Event_Create(Arrival,0,0,3,task)),tail);
-  ListNode_PrintList(tail, "DebugList");  
-
-  ListNode_PrintList(fel->EventList, "Event List");
-
-  for(i=0;i<8;i++)
+  tail=list;
+  for(i=2;i<=SUBTASKS;i++)
   {
-    //printf("Removing first item from the list\n");
-    event = FEL_PopEvent(fel);
-    Event_Destroy(event);
-    //ListNode_PrintList(futureList->EventList, "Event List");
-    
+    tail = ListNode_AppendTail(ListNode_Create(Event_Create(ARRIVAL,0,0,i,task)),tail);
   }
+  
+  list2 = ListNode_Create(Event_Create(ARRIVAL,0,1,1,task2));
+  tail2=list2;
+  for(i=2;i<=SUBTASKS;i++)
+  {
+    tail2 = ListNode_AppendTail(ListNode_Create(Event_Create(ARRIVAL,0,1,i,task2)),tail2);
+  }
+  tail ->Next = list2;
+  tail2->Next = ListNode_Create(Event_Create(DEPARTURE, 2, 1, 1, task));
+  FEL_Append(fel,list);
+
+  //Clean up lists
+  list = NULL;
+  list2 = NULL;
+  
+  ListNode_PrintList(fel->EventList, "FEL");
+
+  list = FEL_Pop(fel);
+  printf("\nPOP1==============\n");
+  ListNode_PrintList(fel->EventList,"FEL");
+  ListNode_PrintList(list, "Popped data");
+  
+  list2 = FEL_Pop(fel);
+  printf("\nPOP2==============\n");
+  ListNode_PrintList(fel->EventList,"FEL");
+  ListNode_PrintList(list2, "Popped data");
+
+  
+  tail = FEL_Pop(fel);
+  printf("\nPOP3==============\n");
+  ListNode_PrintList(fel->EventList,"FEL");
+  ListNode_PrintList(tail, "Popped data");
+
   printf("Ending Test\n\n");
+
   FEL_Destroy(fel);
+  ListNode_DestroyList(list);
+  ListNode_DestroyList(list2);
+  free(tail->Event);
+  free(tail);
+
   return EXIT_SUCCESS;
 }
--- makefile
+++ makefile
@@ -3,3 +3,7 @@ all:
 
 flags = -Wall -Wshadow -g
 source = FEL.c ListNode.c Event.c Queue.c SimulationData.c Server.c Control.c -lm
+
+test:
+	gcc $(flags) $(source) UnitTestSet4.c -o PRJ1_A
+	PRJ1_A
